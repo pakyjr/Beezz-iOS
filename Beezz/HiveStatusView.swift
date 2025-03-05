db914a74fbc53178c293b1ac2bdd24e60290ef2f
AntonioNavarra <navarra.antonio.99@gmail.com>
2025-03-05 12:10:55 +0100

    Project Merged with Lorenzo Pizzuto (HiveStatusView)

---
+++ Beezz/HiveStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  HiveStatusView.swift
+//  Beezz
+//
+//  Created by Antonio Navarra on 05/03/25.
+//
+
+import SwiftUI
+
+struct HiveStatusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HiveStatusView()
+}
