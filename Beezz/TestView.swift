efed298e5e3bcc7f8e12ee8aa047f21067c32a69
AntonioNavarra <navarra.antonio.99@gmail.com>
2025-03-05 14:20:34 +0100

    HiveStatusView

---
+++ Beezz/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Beezz
+//
+//  Created by Antonio Navarra on 05/03/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
